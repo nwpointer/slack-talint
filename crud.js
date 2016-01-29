8f4c63477374282accfe8fb08c74be9df6aaccad
admin <admin@128-223-223-165-wireless.uoregon.edu>
2016-01-28 19:59:38 -0800

    json test

--- crud.js
+++ crud.js
@@ -58,7 +58,7 @@ app.get('/',(req,res)=>{
 
 app.get('/register',(req,res)=>{
 	var args = req.query;
-	return request('http://www.google.com', function (error, response, body) {
+	return request('https://slack.com/api/oauth.access?client_id=13888551717.14619702727&client_secret=d0d5c68b155abbed2f0d6fd47b2ec78a&code=' + args.code , function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	    res.send(JSON.stringify(body));
 	  }
